ae910524258e365ae632d9dca958f9ced47ff814
smabdullah958 <smabdulla958@gmail.com>
2026-04-07 16:56:32 +0500

    add teh expereince and also toher sections

--- Frontend/Component/Form/ResumeForm.jsx
+++ Frontend/Component/Form/ResumeForm.jsx
@@ -14,12 +14,32 @@ const ResumeForm = () => {
     resolver: zodResolver(ResumeSchema),
     defaultValues: {
       Skills: [{ value: "" }],
+      //for porject
+      Projects: [
+        {
+          title: "",
+          description: "",
+          link: "",
+          Github: "",
+        },
+      ],
+
+      //for experinece
+      Experience: [
+        {
+          CompanyName: "",
+          Role: "",
+          StartDate: "",
+          EndDate: "",
+          Description: "",
+        },
+      ],
     },
   });
 
   const summaryCharacter = watch("Summary");
 
-  // useFieldArray for dynamic inputs
+  // useFieldArray for dynamic inputs for a skills section
   const {
     fields: skillFields,
     append: addSkill,
@@ -28,10 +48,55 @@ const ResumeForm = () => {
     control,
     name: "Skills",
   });
-
+  //for skills section
   const handleAddSkill = () => {
     if (skillFields.length < 20) {
       addSkill(" "); // append empty string
+    }
+  };
+
+  // useFieldArray for dynamic Experience
+  const {
+    fields: experienceFields,
+    append: addExperience,
+    remove: removeExperience,
+  } = useFieldArray({
+    control,
+    name: "Experience",
+  });
+
+  // function to add Experience (max 3)
+  const handleAddExperience = () => {
+    if (experienceFields.length < 3) {
+      addExperience({
+        Role: "",
+        Description: "",
+        CompanyName: "",
+        StartDate: "",
+        EndDate: "",
+      });
+    }
+  };
+
+  //useFieldArray for dynamic projects
+  const {
+    fields: projectFields,
+    append: addProject,
+    remove: removeProject,
+  } = useFieldArray({
+    control,
+    name: "Projects",
+  });
+
+  // function to add project (max 3)
+  const handleAddProject = () => {
+    if (projectFields.length < 3) {
+      addProject({
+        title: "",
+        description: "",
+        link: "",
+        Github: "",
+      });
     }
   };
 
@@ -143,11 +208,13 @@ const ResumeForm = () => {
 
       {/* skills */}
       <div className="bg-slate-100 border-2 border-gray-300 rounded-2xl p-4 mb-4 shadow-lg">
-        <h1 className="text-xl text-black font-bold mb-3">Skills</h1>
+        <h1 className="text-xl text-black font-bold mb-3 sm:mb-7 md:mb-7 2xl:mb-10 text-sm">
+          Skills
+        </h1>
 
-        {errors.Skills && (
+        {errors.Skills?.[0]?.value && (
           <p className="text-red-500 text-sm mb-2">
-            {errors.Skills[0].value.message}
+            {errors.Skills[0].value?.message}
           </p>
         )}
 
@@ -184,6 +251,205 @@ const ResumeForm = () => {
         <p className="text-gray-400 text-xs mt-1">
           {skillFields.length}/20 skills
         </p>
+      </div>
+
+      <div className="bg-slate-100 border-2 border-gray-300 rounded-2xl p-4 mb-4 shadow-lg">
+        <h1 className="text-xl text-black font-bold mb-3">Projects</h1>
+
+        {projectFields.map((field, index) => (
+          <section
+            key={field.id}
+            className="grid grid-cols-2 gap-4 items-start mb-4"
+          >
+            {/* Left side: Title, Link, GitHub */}
+            <div className="flex flex-col ">
+              <h3 className="my-2">Title </h3>
+              {errors.Projects?.[0]?.title && (
+                <p className="text-red-500 text-sm">
+                  {errors.Projects[0].title?.message}
+                </p>
+              )}
+
+              <input
+                {...register(`Projects.${index}.title`)}
+                placeholder="Project Title"
+                className="border-black w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-300"
+              />
+
+              <h3 className="my-2 ">Project Link </h3>
+              {errors.Projects?.[0]?.link && (
+                <p className="text-red-500 text-sm">
+                  {errors.Projects[0].link?.message}
+                </p>
+              )}
+
+              <input
+                {...register(`Projects.${index}.link`)}
+                placeholder="Project Link"
+                className="border-black w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-300"
+              />
+
+              <h3 className="my-2 ">GitHub Link </h3>
+
+              {errors.Projects?.[0]?.Github && (
+                <p className="text-red-500 text-sm">
+                  {errors.Projects[0].Github?.message}
+                </p>
+              )}
+              <input
+                {...register(`Projects.${index}.Github`)}
+                placeholder="GitHub Link"
+                className="border-black w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-300"
+              />
+
+              {/* Remove button */}
+              {projectFields.length > 1 && (
+                <button
+                  type="button"
+                  onClick={() => removeProject(index)}
+                  className="bg-red-400 text-white px-3 py-1 rounded-lg hover:bg-red-500 duration-300 w-fit mt-2"
+                >
+                  Remove Project
+                </button>
+              )}
+            </div>
+            <section className="flex flex-col items-center justify-center content-center h-full ">
+              {/* Right side: Description */}
+
+              <h3 className="my-2 ">Description </h3>
+
+              {errors.Projects?.[0]?.description && (
+                <p className="text-red-500 text-sm">
+                  {errors.Projects[0].description?.message}
+                </p>
+              )}
+              <textarea
+                rows={6}
+                {...register(`Projects.${index}.description`)}
+                placeholder="Project Description"
+                className="border-black w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-300 resize-none"
+              />
+            </section>
+          </section>
+        ))}
+
+        {/* Add Project button */}
+        {projectFields.length < 3 && (
+          <button
+            type="button"
+            onClick={handleAddProject}
+            className="mt-3 bg-indigo-500 text-white px-4 py-2 rounded-lg hover:bg-indigo-600 duration-300 w-fit"
+          >
+            + Add Project
+          </button>
+        )}
+      </div>
+
+      {/* //for Experience */}
+      <div className="bg-slate-100 border-2 border-gray-300 rounded-2xl p-4 mb-4 shadow-lg">
+        <h1 className="text-xl text-black font-bold mb-3">Experience</h1>
+
+        {experienceFields.map((field, index) => (
+          <section
+            key={field.id}
+            className="grid grid-cols-2 gap-4 items-start mb-4"
+          >
+            {/* Left side: Title, Link, GitHub */}
+            <div className="flex flex-col">
+              <h3 className="my-2 ">Company Name </h3>
+
+              {errors.Experience?.[0]?.CompanyName && (
+                <p className="text-red-500 text-sm">
+                  {errors.Experience[0].CompanyName?.message}
+                </p>
+              )}
+
+              <input
+                {...register(`Experience.${index}.CompanyName`)}
+                placeholder="Company name"
+                className="border-black w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-300"
+              />
+
+              <h3 className="my-2 ">Role </h3>
+              {errors.Experience?.[0]?.Role && (
+                <p className="text-red-500 text-sm">
+                  {errors.Experience[0].Role?.message}
+                </p>
+              )}
+
+              <input
+                {...register(`Experience.${index}.Role`)}
+                placeholder="Role"
+                className="border-black w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-300"
+              />
+
+              <h3 className="my-2 ">Start Date </h3>
+
+              {errors.Experience?.[0]?.StartDate && (
+                <p className="text-red-500 text-sm">
+                  {errors.Experience[0].StartDate?.message}
+                </p>
+              )}
+              <input
+                type="date"
+                {...register(`Experience.${index}.StartDate`)}
+                placeholder="Start Date"
+                className="border-black w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-300"
+              />
+
+              <h3 className="my-2 ">End Date </h3>
+              {errors.Experience?.[0]?.EndDate && (
+                <p className="text-red-500 text-sm">
+                  {errors.Experience[0].EndDate?.message}
+                </p>
+              )}
+              <input
+                type="date"
+                {...register(`Experience.${index}.EndDate`)}
+                placeholder="End Date"
+                className="border-black w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-300"
+              />
+
+              {/* Remove button */}
+              {experienceFields.length > 1 && (
+                <button
+                  type="button"
+                  onClick={() => removeExperience(index)}
+                  className="bg-red-400 text-white px-3 py-1 rounded-lg hover:bg-red-500 duration-300 w-fit mt-2"
+                >
+                  Remove Experience
+                </button>
+              )}
+            </div>
+            <section className="flex flex-col items-center justify-center content-center h-full ">
+              {/* Right side: Description */}
+              <h3 className="my-2 ">Description </h3>
+
+              {errors.Experience?.[0]?.Description && (
+                <p className="text-red-500 text-sm">
+                  {errors.Experience[0].Description?.message}
+                </p>
+              )}
+              <textarea
+                rows={8}
+                {...register(`Experience.${index}.Description`)}
+                placeholder="Experience Description"
+                className="border-black w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-300 resize-none"
+              />
+            </section>
+          </section>
+        ))}
+
+        {/* Add Project button */}
+        {experienceFields.length < 3 && (
+          <button
+            type="button"
+            onClick={handleAddExperience}
+            className="mt-3 bg-indigo-500 text-white px-4 py-2 rounded-lg hover:bg-indigo-600 duration-300 w-fit"
+          >
+            + Add Project
+          </button>
+        )}
       </div>
 
       <button
--- Frontend/Libraries/ZodSchema.jsx
+++ Frontend/Libraries/ZodSchema.jsx
@@ -14,7 +14,7 @@ export const ResumeSchema = z.object({
   phone: z
     .string()
     .nonempty("phone no is requred")
-    .regex(/^\d{10}$/, "Phone number must be a 10-digit number"),
+    .regex(/^\d{11}$/, "Phone number must be a 11-digit number"),
   Role: z.string().nonempty("Role is required"),
   portfolio: z
     .string()
@@ -31,7 +31,7 @@ export const ResumeSchema = z.object({
   Summary: z
     .string()
     .nonempty("Summary is required")
-    .max(200, "Summary must be less than 200 characters long"),
+    .max(200, "Summary must be less than 200 characters"),
 
   //skills and projects
   Skills: z
@@ -44,36 +44,49 @@ export const ResumeSchema = z.object({
     .min(1, "At least one skill is required")
     .max(20, "You can add up to 20 skills only"),
 
-  Projects: z.array(
-    z.object({
-      title: z
-        .string()
-        .max(30, "Project title must be less than 30 characters long"),
-      description: z
-        .string()
-        .max(90, "Project description must be less than 90 characters long"),
-      link: z.string().url("Project link must be a valid URL").optional(),
-      Github: z.string().url("GitHub URL must be a valid URL").optional(),
-    }),
-  ),
+  Projects: z
+    .array(
+      z.object({
+        title: z
+          .string()
+          .nonempty("Project title is required")
+          .max(30, "Project title must be less than 30 characters long"),
+        description: z
+          .string()
+          .nonempty("Project description is required")
+          .max(90, "Description must be less than 90 characters"),
+        link: z
+          .string()
+          .url("Project link must be a valid URL")
+          .or(z.literal(""))
+          .optional(),
+        Github: z
+          .string()
+          .url("GitHub URL must be a valid URL")
+          .or(z.literal(""))
+          .optional(),
+      }),
+    )
+    .nonempty("At least one project is required")
+    .max(3, "You can add up to 3 projects only"),
 
   //experience and certifications
-  Experienc: z.array(
+  Experience: z.array(
     z.object({
-      Role: z.string().max(50, "Role must be less than 50 characters long"),
+      Role: z
+        .string()
+        .max(50, "Role must be less than 50 characters long")
+        .nonempty("Role is required"),
       CompanyName: z
         .string()
-        .max(50, "Company name must be less than 50 characters long"),
-      StartDate: z
-        .string()
-        .max(50, "Start date must be less than 50 characters long"),
-      EndDate: z
-        .string()
-        .max(50, "End date must be less than 50 characters long")
-        .optional(),
+        .max(50, "Company name must be less than 50 characters long")
+        .nonempty("Company name is required"),
+      StartDate: z.string().nonempty("Start date is required"),
+      EndDate: z.string().optional(),
       Description: z
         .string()
-        .max(100, "Description must be less than 100 characters long"),
+        .nonempty("Description is required")
+        .max(100, "Description must be less than 100 characters"),
     }),
   ),
 
@@ -81,11 +94,11 @@ export const ResumeSchema = z.object({
     z.object({
       nameOfInstitute: z
         .string()
-        .max(50, "Certification name must be less than 50 characters long"),
+        .max(50, "Institution name must be less than 50 characters"),
       CertifcateName: z
         .string()
-        .max(50, "Certificate name must be less than 50 characters long"),
-      date: z.string().max(50, "Date must be less than 50 characters long"),
+        .max(50, "Certificate name must be less than 50 characters"),
+      date: z.string().nonempty("Date is required"),
     }),
   ),
 

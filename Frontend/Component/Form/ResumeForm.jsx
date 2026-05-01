823e9b3d60c24de5c7f71f4abdb8b0f26f76eda1
smabdullah958 <smabdulla958@gmail.com>
2026-05-01 21:10:32 +0500

    adjust the width

--- Frontend/Component/Form/ResumeForm.jsx
+++ Frontend/Component/Form/ResumeForm.jsx
@@ -753,7 +753,7 @@ const ResumeForm = ({ onDataChange }) => {
                     //issue date must be smaller than afuture date
                     max={new Date().toISOString().split("T")[0]}
                     placeholder="Issue Date"
-                    className="border border-gray-400 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-300 text-gray-500"
+                    className="border border-gray-400 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-300 text-gray-500 w-full"
                   />
                 </div>
               </div>
